d7b69f709ed3eda1ad893820717f3bf84baa327b
Tran Dinh Thang <trandinhthang99.hy@gmail.com>
2022-06-22 18:37:49 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AuthContextProvider from "./contexts/AuthContext";
-import {BrowserRouter} from "react-router-dom";
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+// import AuthContextProvider from "./contexts/AuthContext";
+// import {BrowserRouter} from "react-router-dom";
+// import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,7 +15,9 @@ root.render(
       {/*        <App/>*/}
       {/*    </BrowserRouter>*/}
       {/*</AuthContextProvider>*/}
-      <App/>
+      <div>
+          Test Deploy
+      </div>
   </React.StrictMode>
 );
 
