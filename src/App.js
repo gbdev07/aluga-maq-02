c5d7efd0c27bb864241329d231a43ff4f07c6c48
Tran Dinh Thang <trandinhthang99.hy@gmail.com>
2022-06-22 20:18:58 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Route, Routes} from "react-router-dom";
 import NotFound from  "./components/NotFound/NotFound";
 import {useContext} from "react";
 import {AuthContext} from "./contexts/AuthContext";
-import SignUp from "./components/Auth/SignUp/SignUp";
+// import SignUp from "./components/Auth/SignUp/SignUp";
 // import SignIn from "./components/Auth/SignIn/SignIn";
 // import PublicRoute from "./routes/PublicRoute";
 // import Main from "./components/Layout/Main/Main";
