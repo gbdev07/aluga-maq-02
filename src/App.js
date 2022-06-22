1f8f6c491f404615f770e889aeadf61c59e46d60
Tran Dinh Thang <trandinhthang99.hy@gmail.com>
2022-06-22 11:12:02 +0700

    signup page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-
+import PrivateRoute from "./routes/PrivateRoute";
+import {Route, Switch} from "react-router-dom";
+import NotFound from "./components/NotFound/NotFound";
+import SignUp from "./components/Auth/SignUp/SignUp";
+import SignIn from "./components/Auth/SignIn/SignIn";
+import PublicRoute from "./routes/PublicRoute";
+import Main from "./components/Layout/Main/Main";
+import Dashboard from "./components/Main/Dashboard/Dashboard";
+import {DASHBOARD} from "./utils/links";
+import * as links from "./utils/links"
+import {CANAL, FORNECEDOR} from "./utils/constants";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Fit2sell - Em construção...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Switch>
+            <PrivateRoute path={links.DASHBOARD}>
+                <Main>
+                    <Dashboard />
+                </Main>
+            </PrivateRoute>
+            <PublicRoute path={links.SIGNUP_CANAL}>
+                <SignUp
+                    type={CANAL}
+                />
+            </PublicRoute>
+            <PublicRoute path={links.SIGNUP_FORNECEDOR}>
+                <SignUp
+                    type={FORNECEDOR}
+                />
+            </PublicRoute>
+            <PublicRoute path={links.SIGNIN_CANAL}>
+                <SignIn
+                    type={CANAL}
+                />
+            </PublicRoute>
+            <PublicRoute path={links.SIGNIN_FORNECEDOR}>
+                <SignIn
+                    type={FORNECEDOR}
+                />
+            </PublicRoute>
+            <Route component={NotFound} />
+        </Switch>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AuthContextProvider from "./contexts/AuthContext";
+import {BrowserRouter} from "react-router-dom";
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <AuthContextProvider>
+          <BrowserRouter>
+              <App/>
+          </BrowserRouter>
+      </AuthContextProvider>
   </React.StrictMode>
 );
 
