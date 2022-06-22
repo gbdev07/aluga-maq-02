554724f76a58a292b9020abe7c31d42d5caa9ed0
Tran Dinh Thang <trandinhthang99.hy@gmail.com>
2022-06-22 19:18:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import PrivateRoute from "./routes/PrivateRoute";
-// import {Route, Routes} from "react-router-dom";
+// import PrivateRoute from "./routes/PrivateRoute";
+import {Route, Routes} from "react-router-dom";
 import NotFound from  "./components/NotFound/NotFound";
 // import SignUp from "./components/Auth/SignUp/SignUp";
 // import SignIn from "./components/Auth/SignIn/SignIn";
