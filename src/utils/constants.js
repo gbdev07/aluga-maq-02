c05eb04805ae7d5a6ead080b9ef9dc68a87f49c5
Tran Dinh Thang <trandinhthang99.hy@gmail.com>
2022-06-22 18:48:02 +0700

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
 export const CANAL = 'CANAL';
 export const FORNECEDOR = 'FORNECEDOR';
-export const REACT_APP_API_BASE_URL = 'https://fit2sell.herokuapp.com'
-// export const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+export const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
